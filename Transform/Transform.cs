1530141498fe8a776f9bb39a6580c2b3cdb9279c
yamini priya <yaminipriyakodeboyina@gmail.com>
2018-07-19 23:03:52 +0530

    first commit

--- Transform/Transform.cs
+++ Transform/Transform.cs
@@ -6,9 +6,24 @@ namespace Transform
 {
     public static class Transform 
     {
-        public static void Map()
+        public static int doublenum(int item)
         {
-            throw new NotImplementedException();
+            item = item * 2;
+            return item;
+        }
+        public static List<int> Map()
+        {
+            List<int> numbers= new List<int>();
+            List<int> numbers1= new List<int>();
+             numbers.Add(1);
+			numbers.Add(2);
+            numbers.Add(3);
+            numbers.Add(5);
+            foreach (int item in numbers)
+	        {
+                numbers.Add(doublenum(item));
+	        }
+            return numbers;
         }
     }
 }
