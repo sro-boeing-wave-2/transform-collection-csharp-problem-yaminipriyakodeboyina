efcbd77da25b109718a482e78fd82e992cf33e31
yamini priya <yaminipriyakodeboyina@gmail.com>
2018-07-20 00:10:58 +0530

    first commit

--- Transform/Transform.cs
+++ Transform/Transform.cs
@@ -6,22 +6,13 @@ namespace Transform
 {
     public static class Transform 
     {
-        public static int doublenum(int item)
+        
+        public static List<int> Map(this int[] num, Func<int,int>twice)
         {
-            item = item * 2;
-            return item;
-        }
-        public static List<int> Map()
-        {
-            List<int> numbers= new List<int>();
-            List<int> numbers1= new List<int>();
-             numbers.Add(1);
-			numbers.Add(2);
-            numbers.Add(3);
-            numbers.Add(5);
-            foreach (int item in numbers)
+            List <int> numbers= new List<int>();
+            foreach (int item in num)
 	        {
-                numbers.Add(doublenum(item));
+                numbers.Add(twice(item));
 	        }
             return numbers;
         }
